4be46c4a28a6cac2f5fab69e1e9d35292e7108dc
David Singleton <davidsingleton@gmail.com>
2014-09-13 19:41:44 -0700

    Add basic Auth0 support

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import json
 import os
 import redis
 import requests
-from flask import Flask, render_template
+from flask import Flask, request, jsonify, session, redirect, render_template, send_from_directory
 
 app = Flask(__name__)
 app.debug = True
@@ -19,6 +19,47 @@ class Backend(object):
 
 backend = Backend()
 
+@app.route('/callback')
+def callback_handling():
+  env = os.environ
+  code = request.args.get('code')
+
+  json_header = {'content-type': 'application/json'}
+
+  token_url = "https://{domain}/oauth/token".format(domain=env["AUTH0_DOMAIN"])
+  token_payload = {
+    'client_id' : env['AUTH0_CLIENT_ID'], \
+    'client_secret' : env['AUTH0_CLIENT_SECRET'], \
+    'redirect_uri' : env['AUTH0_CALLBACK_URL'], \
+    'code' : code, \
+    'grant_type': 'authorization_code' \
+  }
+
+  token_info = requests.post(token_url, data=json.dumps(token_payload), headers = json_header).json()
+
+  user_url = "https://{domain}/userinfo?access_token={access_token}"  \
+    .format(domain=env["AUTH0_DOMAIN"], access_token=token_info['access_token'])
+
+  user_info = requests.get(user_url).json()
+
+  # We're saving all user information into the session
+  session['profile'] = user_info
+
+  # Redirect to the User logged in page that you want here
+  # In our case it's /dashboard
+  return redirect('/dashboard')
+
+def requires_auth(f):
+  @wraps(f)
+  def decorated(*args, **kwargs):
+    if not session.has_key('profile'):
+      # Redirect to Login page here
+      return redirect('/')
+    return f(*args, **kwargs)
+
+  return decorated
+
+# The app itself...  
 @app.route('/api/id/<tiw_id>')
 def api_get_by_id(tiw_id):
   response = backend.tiw_by_id(tiw_id)
@@ -26,9 +67,9 @@ def api_get_by_id(tiw_id):
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return render_template('index.html', user=session['profile'])
 
 @app.route('/tiw/<tiw_id>')
 def permalink(tiw_id):
   tiw = backend.tiw_by_id(tiw_id)
-  return render_template('permalink.html', tiw=tiw)
+  return render_template('permalink.html', tiw=tiw, user=session['profile'])
